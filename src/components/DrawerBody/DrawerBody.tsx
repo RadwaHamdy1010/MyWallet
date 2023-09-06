48cdc7ae3289f4422d127763077ba7b1525fff07
RadwaHamdy1010 <radwa.mohamed4@vodafone.com>
2023-09-07 00:07:35 +0300

    feat: add custom drawer body

--- src/components/DrawerBody/DrawerBody.tsx
+++ src/components/DrawerBody/DrawerBody.tsx
@@ -1,20 +1,47 @@
 import React from 'react';
-import {Text, StatusBar, SafeAreaView} from 'react-native';
 import {
   DrawerContentScrollView,
   DrawerItemList,
+  DrawerItem,
 } from '@react-navigation/drawer';
+import {
+  DrawerActions,
+  DrawerNavigationState,
+  ParamListBase,
+} from '@react-navigation/native';
 
-export default class DrawerBody extends React.Component {
+import {Text} from 'react-native';
+import {
+  DrawerNavigationHelpers,
+  DrawerDescriptorMap,
+} from '@react-navigation/drawer/lib/typescript/src/types';
+
+interface DrawerBodyProps {
+  state: DrawerNavigationState<ParamListBase>;
+  navigation: DrawerNavigationHelpers;
+  descriptors: DrawerDescriptorMap;
+}
+export default class DrawerBody extends React.Component<DrawerBodyProps> {
   render(): React.ReactNode {
-    console.log('homeScreen', this.props.navigation);
+    console.log('homeScreen', this.props.descriptors);
 
     return (
-      <>
-        <SafeAreaView />
-        <StatusBar />
-        <DrawerItemList navigation={this.props.navigation} />
-      </>
+      <DrawerContentScrollView {...this.props}>
+        <Text>weeewe</Text>
+        <DrawerItemList {...this.props} />
+        <DrawerItem
+          label="Close drawer"
+          onPress={() =>
+            this.props.navigation.dispatch(DrawerActions.closeDrawer())
+          }
+        />
+        <DrawerItem
+          label="Toggle drawer"
+          onPress={() =>
+            this.props.navigation.dispatch(DrawerActions.toggleDrawer())
+          }
+        />
+      </DrawerContentScrollView>
     );
   }
 }
--- src/navigation/DrawerNavigator/DrawerNavigator.tsx
+++ src/navigation/DrawerNavigator/DrawerNavigator.tsx
@@ -1,35 +1,43 @@
 import {JSX} from 'react';
+import {DrawerActions} from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {DrawerNavigationHelpers} from '@react-navigation/drawer/lib/typescript/src/types';
+
+import {Image, TouchableOpacity} from 'react-native';
 import {HomeScreen, AddExpense} from '../../screens';
 import {DrawerBody} from '../../components';
-import {Text, View, Button, Image, ScrollView} from 'react-native';
 import images from '../../Assets';
 
 const Drawer = createDrawerNavigator();
 
-const renderLeftIcon = () => (
-  <Image
-    source={images.drawerIcon}
-    style={{width: 25, height: 25, marginLeft: 10}}
-  />
-);
+type DrawerProps = {
+  navigation: DrawerNavigationHelpers;
+};
 
-export default function DrawerNavigator(props): JSX.Element {
-  console.log(props.descriptors);
+export default function DrawerNavigator({
+  navigation,
+}: DrawerProps): JSX.Element {
   return (
     <Drawer.Navigator
       initialRouteName="HomeScreen"
-      // screenOptions={{
-      //   drawerType: 'slide',
-      //   //   headerLeft: renderLeftIcon,
-      // }}
-    >
-      <Drawer.Screen
-        name="HomeScreen"
-        component={HomeScreen}
-       // options={{headerLeft: renderLeftIcon}}
-      />
+      drawerContent={props => <DrawerBody {...props} />}
+      screenOptions={{
+        drawerType: 'slide',
+        headerLeft: () => renderLeftIcon(navigation),
+      }}>
+      <Drawer.Screen name="HomeScreen" component={HomeScreen} />
       <Drawer.Screen name="AddExpense" component={AddExpense} />
     </Drawer.Navigator>
+  );
+}
+
+function renderLeftIcon({dispatch}: DrawerNavigationHelpers) {
+  return (
+    <TouchableOpacity onPress={() => dispatch(DrawerActions.openDrawer())}>
+      <Image
+        source={images.drawerIcon}
+        style={{width: 25, height: 25, marginLeft: 10}}
+      />
+    </TouchableOpacity>
   );
 }
